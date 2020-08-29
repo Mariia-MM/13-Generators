d789b4fc25f2b2378da36920f8c72a700274430d
Mariia <mytsko.mariia@gmail.com>
2020-08-29 18:22:10 +0200

    advance task

--- index.js
+++ index.js
@@ -6,8 +6,26 @@ const idGenerator = createIdGenerator();
 
 idGenerator.next().value;
 idGenerator.next().value;
-idGenerator.next().value;
-idGenerator.next().value;
-idGenerator.next().value;
-idGenerator.next().value;
-idGenerator.next().value;
+
+function* newFontGenerator(size) {
+  while (true) {
+    let sign = yield size;
+    if (sign == `up`) {
+      console.log(`font size:`, (size = size + 2));
+    } else if (sign == `down`) {
+      console.log(`font size:`, (size = size - 2));
+    } else {
+      console.log(`font size:`, size);
+    }
+  }
+}
+const fontGenerator = newFontGenerator(14);
+
+fontGenerator.next(`up`).value;
+fontGenerator.next(`up`).value;
+fontGenerator.next(`up`).value;
+fontGenerator.next().value;
+fontGenerator.next("down").value;
+fontGenerator.next(`down`).value;
+fontGenerator.next("down").value;
+fontGenerator.next().value;
