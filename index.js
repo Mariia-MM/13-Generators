6fb05a6f1e6a1cfe1320ab8bf049383ab7743696
Mariia <mytsko.mariia@gmail.com>
2020-08-29 19:47:19 +0200

    styling

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const genButton = document.getElementById("idButton");
+const genValue = document.getElementById("idValue");
+const bigSize = document.getElementById("bigger");
+const smallSize = document.getElementById("smaller");
+const sameSize = document.getElementById("size");
+const textEx = document.getElementById("text");
+
 function* createIdGenerator() {
   let id = 0;
-  while (true) yield console.log(`id:`, ++id);
+  while (true) yield ++id;
 }
 const idGenerator = createIdGenerator();
 
-idGenerator.next().value;
-idGenerator.next().value;
+genButton.addEventListener(`click`, function () {
+  genValue.innerHTML = idGenerator.next().value;
+});
 
 function* newFontGenerator(size) {
   while (true) {
@@ -21,11 +29,13 @@ function* newFontGenerator(size) {
 }
 const fontGenerator = newFontGenerator(14);
 
-fontGenerator.next(`up`).value;
-fontGenerator.next(`up`).value;
-fontGenerator.next(`up`).value;
-fontGenerator.next().value;
-fontGenerator.next("down").value;
-fontGenerator.next(`down`).value;
-fontGenerator.next("down").value;
-fontGenerator.next().value;
+bigSize.addEventListener(`click`, function () {
+  textEx.style.fontSize = fontGenerator.next(`up`).value + `px`;
+});
+
+smallSize.addEventListener(`click`, function () {
+  textEx.style.fontSize = fontGenerator.next(`down`).value + `px`;
+});
+sameSize.addEventListener(`click`, function () {
+  textEx.style.fontSize = fontGenerator.next().value + `px`;
+});
