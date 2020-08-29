2e552bc3cff60e3500ae6fe9d337678321b295fa
Mariia <mytsko.mariia@gmail.com>
2020-08-29 17:12:19 +0200

    task1

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 function* createIdGenerator() {
   let id = 0;
-  while (true) yield ++id;
+  while (true) yield console.log(`id:`, ++id);
 }
 const idGenerator = createIdGenerator();
 
-console.log(idGenerator.next().value);
-console.log(idGenerator.next().value);
-
-console.log(idGenerator.next().value);
-
-console.log(idGenerator.next().value);
-
-console.log(idGenerator.next().value);
-
-console.log(idGenerator.next().value);
-
-// idGenerator.next().value;
-// idGenerator.next().value;
+idGenerator.next().value;
+idGenerator.next().value;
+idGenerator.next().value;
+idGenerator.next().value;
+idGenerator.next().value;
+idGenerator.next().value;
+idGenerator.next().value;
